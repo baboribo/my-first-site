a922c53238f389dc1509330425ea02df93c01e26
SpectralGlint <71020988+Monokii@users.noreply.github.com>
2024-11-30 13:12:54 +0900

    업데이트 - 요소 추가

--- index.js
+++ index.js
@@ -2,14 +2,61 @@
 import anime from "https://cdn.jsdelivr.net/npm/animejs/lib/anime.es.js";
 
 anime({
-  targets: '.ui-content-inbox',
-  translateY: [250, 0], // from 100 to 250
+  targets: '.ui-content-grid',
+  translateX: [80, 0],
   duration: 800,
   opacity: [0, 1],
-  rotate: [20, 0],
+  rotate: [0, 0],
+  delay: '300',
+  easing: 'easeOutExpo'
 }); 
 anime({
   targets: '.homehome',
-  rotate: 360,
-  loop: true
+  translateX: [50, 0],
+  opacity: [0, 1],
+  delay: '10ms',
+  duration: 800,
+  easing: 'easeOutExpo'
+}); 
+anime({
+  targets: '.homehome2',
+  translateX: [30, 0],
+  opacity: [0, 1],
+  delay: '180ms',
+  duration: 800,
+  easing: 'easeOutExpo'
+}); 
+anime({
+  targets: '.ui-gnb',
+  translateY: [-40, 0],
+  opacity: [0, 1],
+  duration: 500,
+  easing: 'easeOutExpo',
+  delay: '800ms',
+}); 
+
+anime({
+  targets: '.hr-a',
+  opacity: [0, 1],
+  duration: 800,
+  easing: 'easeOutExpo',
+  delay: '1200ms',
+}); 
+
+anime({
+  targets: 'h2',
+  opacity: [0, 1],
+  translateX: [40, 0],
+  duration: 800,
+  easing: 'easeOutExpo',
+  delay: '240ms',
+}); 
+
+anime({
+  targets: '.bottom-text-0',
+  opacity: [0, 1],
+  translateY: [40, 0],
+  duration: 800,
+  easing: 'easeOutExpo',
+  delay: '2000ms',
 }); 
